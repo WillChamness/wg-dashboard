7d8e175cdb1d99f0127e6590c7ebd54facf8fb15
WillChamness <wzchamness@gmail.com>
2024-06-01 16:04:22 -0400

    feat: add page to view user profiles
    if admin, view all profiles. otherwise, view only user's profile

--- WgDashboardWebsite/Helpers/StringNullOrEmpty.cs
+++ WgDashboardWebsite/Helpers/StringNullOrEmpty.cs
@@ -2,14 +2,14 @@
 {
     public static class StringsNullOrEmpty
     {
-        public static bool CheckStrings(params string?[] strings)
+        public static bool Any(params string?[] strings)
         {
             for(int i = 0; i < strings.Length; i++)
             {
                 if (string.IsNullOrEmpty(strings[i]))
-                    return false;
+                    return true;
             }
-            return true;
+            return false;
         }
     }
 }
--- WgDashboardWebsite/Models/UserProfile.cs
+++ WgDashboardWebsite/Models/UserProfile.cs
@@ -12,4 +12,13 @@ namespace WgDashboardWebsite.Models
         public string Username { get; set; } = "";
         public string Role { get; set; } = UserRoles.Anonymous;
     }
+
+    public static class UserRoles
+    {
+        public static readonly string Anonymous = "anonymous";
+        public static readonly string User = "user";
+        public static readonly string Admin = "admin";
+
+        public static bool IsValidRole(string? role) => (role == Anonymous) || (role == User) || (role == Admin);
+    }
 }
