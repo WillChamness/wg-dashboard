de332bc776bafcb74c94375e222a40837cea1dfb
WillChamness <wzchamness@gmail.com>
2024-06-28 20:15:25 -0400

    test: add more tests for user endpoint

--- WgDashboard.Tests/test_api_users.py
+++ WgDashboard.Tests/test_api_users.py
@@ -1,6 +1,8 @@
 import os
+import pprint
 import random
 import unittest
+from urllib import request
 import dotenv
 import requests
 import jwt
@@ -39,7 +41,7 @@ class TestApiUsers(unittest.TestCase):
     
 
     @staticmethod
-    def _login(url, username: str, password: str) -> tuple[str, dict[str, str]]:
+    def _login(url: str, username: str, password: str) -> tuple[str, dict[str, str]]:
         assert len(username) > 0
         assert len(password) > 0
         response = requests.post(url + "/api/auth/login", json={
@@ -70,6 +72,7 @@ class TestApiUsers(unittest.TestCase):
         self.url: str = os.getenv("API_URL") 
         self.jwt1, self.user1 = self._signup_and_login(self.url)
         self.jwt2, self.user2 = self._signup_and_login(self.url)
+        self.jwt3, self.user3 = self._signup_and_login(self.url)
         self.admin_jwt, self.admin = self._login(self.url, "admin", "admin")
 
         self.url += "/api/users"
@@ -120,7 +123,8 @@ class TestApiUsers(unittest.TestCase):
 
         body = json.loads(response.content.decode())
         self.assertIsInstance(body, list)
-        self.assertTrue(all([isinstance(user, dict) for user in body]))
+        for user in body:
+            self.assertIsInstance(user, dict)
 
 
     def test_access_other_user_with_admin(self):
@@ -131,6 +135,116 @@ class TestApiUsers(unittest.TestCase):
 
         body = json.loads(response.content.decode())
         self.assertIsInstance(body, dict)
+
+
+    def test_change_username(self):
+        new_username = "myupdateduser" + str(random.randint(0, 10**9))
+
+        response = requests.put(self.url + "/" + self.user1["sid"],
+            headers={"Authorization": "Bearer " + self.jwt1},
+            json={
+                "id": self.user1["sid"],
+                "username": new_username,
+                "role": "user",
+                "name": "mynewuser"
+            }
+        )
+        self.assertTrue(200 <= response.status_code and response.status_code <= 299)
+
+        response = requests.get(self.url + "/" + self.user1["sid"],
+            headers={"Authorization": "Bearer " + self.jwt1}
+        )
+        self.assertTrue(200 <= response.status_code and response.status_code <= 299)
+
+        body = json.loads(response.content.decode())
+        body = { key.lower(): value for key, value in body.items() }
+
+        self.assertEqual(body["username"], new_username)
+
+
+    def test_change_username_already_exists(self):
+        new_username = self.user2["nameidentifier"]
+        
+        response = requests.put(self.url + "/" + self.user1["sid"],
+            headers={"Authorization": "Bearer " + self.jwt1},
+            json={
+                "id": self.user1["sid"],
+                "username": new_username,
+                "role": "user",
+                "name": "mynewuser",
+            }
+        )
+
+        self.assertTrue(400 <= response.status_code and response.status_code <= 499)
+
+
+    def test_change_another_username(self):
+        new_username = "someusername"
+        
+        response = requests.put(self.url + "/" + self.user1["sid"],
+            headers={"Authorization": "Bearer " + self.jwt2}, # unauthorized user
+            json={
+                "id": self.user1["sid"],
+                "username": new_username,
+                "role": "user",
+                "name": "mynewuser",
+            }
+        )
+
+        self.assertTrue(400 <= response.status_code and response.status_code <= 499)
+
+
+    def test_change_role_without_admin(self):
+        response = requests.put(self.url + "/" + self.user1["sid"],
+            headers={"Authorization": "Bearer " + self.jwt1}, 
+            json={
+                "id": self.user1["sid"],
+                "username": self.user1["nameidentifier"],
+                "role": "admin", # escalating own priviliges as a user
+                "name": self.user1["name"],
+            }
+        )
+        self.assertTrue(400 <= response.status_code and response.status_code <= 499)
+
+        # check to make sure nothing was changed
+        response = requests.get(self.url + "/" + self.user1["sid"],
+            headers={"Authorization": "Bearer " + self.jwt1}
+        )
+        body = json.loads(response.content.decode())
+        body = { key.lower(): value for key, value in body.items() }
+        self.assertTrue(body["role"], "user")
+
+
+    def test_change_role_with_admin(self):
+        response = requests.put(self.url + "/" + self.user3["sid"],
+            headers={"Authorization": "Bearer " + self.admin_jwt},
+            json={
+                "id": self.user3["sid"],
+                "username": self.user3["nameidentifier"],
+                "role": "admin",
+                "name": self.user1["name"],
+            }
+        )
+        self.assertTrue(200 <= response.status_code and response.status_code <= 299)
+
+        # check to make sure user is now admin
+        response = requests.get(self.url + "/" + self.user3["sid"],
+            headers={"Authorization": "Bearer " + self.admin_jwt}
+        )
+        self.assertTrue(200 <= response.status_code and response.status_code <= 299)
+        body = json.loads(response.content.decode())
+        body = { key.lower(): value for key, value in body.items() }
+
+        self.assertIsInstance(body, dict)
+        self.assertEqual(body["role"], "admin")
+
+
+
+
+
+        
+
+        
 
     
 
